692f65c868fd25806ee7bb3a3cef92156fcaa934
Ori Rawlings <orirawlings@gmail.com>
2017-01-24 13:05:45 -0600

    Exercise 1.2

---
+++ ch1/echo5/main.go
@@ -0,0 +1,13 @@
+// Echo5 prints its command-line arguments, one per line with index number.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for i, arg := range os.Args {
+		fmt.Println(i, arg)
+	}
+}
