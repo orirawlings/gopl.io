cfb6269a58b9b32b3b690307ed7213fc6da4b318
Ori Rawlings <orirawlings@gmail.com>
2017-01-30 17:20:20 -0600

    Exercise 1.8
    Create fetch3 which will append the http protocol to command line
    arguments if it isn't initially provided with an http or https protocol.

---
+++ ch1/fetch3/main.go
@@ -0,0 +1,30 @@
+// Fetch prints the content found at each specified URL without buffering all the content in memory,
+// and will append "http://" to the command line argument if it isn't already prefixed with "http://" or "https://".
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
