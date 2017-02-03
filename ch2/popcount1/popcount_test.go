59191fc4f626a78492081ba48213ace7590650ba
Ori Rawlings <orirawlings@gmail.com>
2017-02-03 17:37:27 -0600

    Exercise 2.4
    Add shifting based implementation of popcount

--- ch2/popcount1/popcount_test.go
+++ ch2/popcount1/popcount_test.go
@@ -10,6 +10,11 @@ func ExamplePopCount() {
 	// Output: 32
 }
 
+func ExamplePopCountShift() {
+	fmt.Println(PopCountShift(0x123456789ABCDEF))
+	// Output: 32
+}
+
 func BenchmarkPopCount(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		PopCount(0x1234567890ABCDEF)
@@ -19,5 +24,11 @@ func BenchmarkPopCount(b *testing.B) {
 func BenchmarkPopCountLoop(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		PopCountLoop(0x1234567890ABCDEF)
+	}
+}
+
+func BenchmarkPopCountShift(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountShift(0x1234567890ABCDEF)
 	}
 }
