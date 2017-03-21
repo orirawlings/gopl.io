12cb19e2255f760a53bddf7f923b5408e5445a9c
Ori Rawlings <orirawlings@gmail.com>
2017-03-21 09:18:03 -0500

    Exercise 3.7
    Render approximation of roots of f(x) = x^4 - 1 using Newton's method to
    various points in the complex plane.

---
+++ ch3/newton/main.go
@@ -0,0 +1,49 @@
+// Newton emits a PNG image of the roots of f(x) = x^4 - 1 by applying Newton's method to points in the complex plane.
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"os"
+)
+
+const (
+	xmin, ymin, xmax, ymax = -2, -2, +2, +2
+	width, height          = 1024, 1024
+)
+
+func main() {
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// Image point (px, py) represents complex value z.
+			img.Set(px, py, newton(z))
+		}
+	}
+	png.Encode(os.Stdout, img) // NOTE: ignoring errors
+}
+
+// f(x) = x^4 - 1
+//
+// z' = z - f(z)/f'(z)
+//    = z - (z^4 - 1) / (4 * z^3)
+//    = z - (z - 1/z^3) / 4
+func newton(z complex128) color.Color {
+	const iterations = 37
+	const contrast = 7
+	for i := uint8(0); i < iterations; i++ {
+		z -= (z - 1/(z*z*z)) / 4
+		if cmplx.Abs(z*z*z*z-1) < 1e-6 {
+			// Color based on which root we've found (i.e. one of {-1, 1, -i, i})
+			blue := uint8(255 * (real(z) + 1) / 2)
+			red := uint8(255 * (imag(z) + 1) / 2)
+			return color.RGBA{red, 255 - contrast*i, blue, 0xff}
+		}
+	}
+	return color.Black
+}
